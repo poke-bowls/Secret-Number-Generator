8932c143b27dd9d91696ec6ed5f48dcd2cc63243
Micah Koki <micah.koki@gmail.com>
2015-12-04 00:45:03 -0800

    Finished Secret Number Generator

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,7 @@
-'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  'use strict';
+  var number = Math.random() * ((1000000 - 0) + 1) + 0;
+  return function secretNumber (){
+    return number;
+  };
 };
